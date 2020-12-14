1288761c12b22b504c275eaf0d0a6892c346b5e9
liucg <164445438@qq.com>
2020-12-14 18:24:35 +0800

    model

--- src/Models/Upload.php
+++ src/Models/Upload.php
@@ -2,13 +2,11 @@
 
 namespace Liucg1995\Uploader\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Upload extends Model
 {
-    use HasFactory;
     use SoftDeletes;
 
     /**
